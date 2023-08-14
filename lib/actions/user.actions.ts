27ca892f44112f62394174b31c986a0080f4fe51
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-08-14 23:36:07 +0530

    profile page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface Params {
     userId: string;
@@ -59,3 +60,29 @@ interface Params {
     }   
   }
   
+
+  export async function fetchUserPosts(userId: string) {
+    try {
+      connectToDB()
+
+      const threads = await User.findOne({ id: userId})
+      .populate({
+        path: 'threads',
+        model: Thread,
+        populate: {
+          path: 'children',
+          model: Thread,
+          populate: {
+            path: 'author',
+            model: User,
+            select: 'name image id'
+          }
+        }
+      })
+
+      return threads;
+    } catch (error : any) {
+      throw new Error(`Failed to fetch user threads ${error.message }`)
+      
+    }
+  }
