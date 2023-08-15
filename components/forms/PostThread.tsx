fecff04b6990067f0e6f0e22df570029f7255cbf
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-08-15 12:54:49 +0530

    add organizations

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -15,9 +15,9 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
+import { useOrganization } from "@clerk/nextjs";
 
 interface Props {
   userId: string;
@@ -26,7 +26,8 @@ interface Props {
 function PostThread({ userId }: Props) {
   const router = useRouter();
   const pathname = usePathname();
-
+  const { organization } = useOrganization()
+ 
 
   const form = useForm<z.infer<typeof ThreadValidation>>({
     resolver: zodResolver(ThreadValidation),
@@ -37,13 +38,16 @@ function PostThread({ userId }: Props) {
   });
 
   const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
-    await createThread({ 
+
+      await createThread({ 
         text: values.thread,
         author: userId,
-        communityId: null,
+        communityId: organization ? organization.id : null,
         path: pathname
     })
 
+  
+   
     router.push("/")
   };
 
